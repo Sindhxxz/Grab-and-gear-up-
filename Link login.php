5215d6f608632483212cc561a23def6bcb9b3bf0
Sindhxxz <93997363+Sindhxxz@users.noreply.github.com>
2021-11-24 16:15:43 +0530

    Add files via upload
    Linking login page to database

---
+++ Link login.php
@@ -0,0 +1,48 @@
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $Username = $_POST['Username'];
+        $Password = $_POST['Password'];
+        
+      
+      // Connecting to the Database
+      $servername = "localhost";
+      $username = "root";
+      $password = "";
+      $database = "loginpage";
+
+      // Create a connection
+      $conn = mysqli_connect($servername, $username, $password, $database);
+      // Die if connection was not successful
+      if (!$conn){
+          die("Sorry we failed to connect: ". mysqli_connect_error());
+      }
+      else{ 
+        // Submit these to a database
+        // Sql query to be executed 
+        $sql = "INSERT INTO `loginpage` (`Username`, `Password`) VALUES ('$Username', '$Password', current_timestamp())";
+        $result = mysqli_query($conn, $sql);
+ 
+        if($result){
+          echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+          <strong>Success!</strong> Your entry has been submitted successfully!
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>';
+        }
+        else{
+            // echo "The record was not inserted successfully because of this error ---> ". mysqli_error($conn);
+            echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+          <strong>Error!</strong> We are facing some technical issue and your entry ws not submitted successfully! We regret the inconvinience caused!
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>';
+        }
+
+      }
+
+    }
+
+    
+?>
